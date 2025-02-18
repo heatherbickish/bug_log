5a6f9662a6d08ba96d4d093a7c79fa8298ae0284
heather bickish <hbickish@gmail.com>
2025-02-18 11:22:24 -0700

    init

---
+++ client/src/models/Account.js
@@ -0,0 +1,19 @@
+export class Account {
+  /**
+   * @typedef AccountData
+   * @property {string} id
+   * @property {string} email
+   * @property {string} name
+   * @property {string} picture
+   * 
+   * @param {AccountData} data
+   */
+  constructor(data) {
+    this.id = data.id
+    this.email = data.email
+    this.name = data.name
+    this.picture = data.picture
+    this.butt = data.butt
+    // TODO add additional properties if needed
+  }
+}
