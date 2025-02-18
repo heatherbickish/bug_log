0ef1c21bf84646bb58cd2b3131585e47457b93c2
heather bickish <hbickish@gmail.com>
2025-02-18 13:20:57 -0700

    deleting note

--- server/src/services/NotesService.js
+++ server/src/services/NotesService.js
@@ -1,6 +1,14 @@
 import { dbContext } from "../db/DbContext"
+import { Forbidden } from "../utils/Errors"
 
 class NotesService {
+  async deleteNote(noteId, userId) {
+    const noteToDelete = await dbContext.Notes.findById(noteId)
+    if (noteToDelete == null) { throw new Error(`Invalid note id: ${noteId}`) }
+    if (noteToDelete.creatorId != userId) { throw new Forbidden("CANT DO THAT IT DONT BELONG TO YOU") }
+    await noteToDelete.deleteOne()
+    return 'Note has been deleted'
+  }
   async getAllNotesByBugId(bugId) {
     const notes = await dbContext.Notes.find({ bugId: bugId })
     if (notes == null) { throw new Error(`Invalid your face: ${bugId}`) }
