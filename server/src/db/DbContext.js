44916342d8dc3bf06c7e348bd7c01f87f8baadbb
heather bickish <hbickish@gmail.com>
2025-03-18 14:10:37 -0600

    note schema, reg in db

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { BugSchema } from "../models/Bug.js";
+import { NoteSchema } from "../models/Note.js";
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Bugs = mongoose.model('Bug', BugSchema);
+  Notes = mongoose.model('Note', NoteSchema);
+
 
 }
 
